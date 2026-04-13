56801270b83976d6d048fb0228d4d82e7ce3936f
jakubwj2 <jakubwj2@gmail.com>
2026-04-13 22:19:17 +0100

    Update converting smart tutor database to csv (add question ids)

--- backend/cli/db_to_csv.py
+++ backend/cli/db_to_csv.py
@@ -1,18 +1,39 @@
 import os
 
 import pandas as pd
+from sqlalchemy.orm import joinedload
 
 from app import create_app, db
-from models.problem_log import ProblemLog
+from models import ProblemLog, Question
 
 DIR = "pykt-toolkit/data/smart_tutor"
 
 
 def main():
     with create_app().app_context():
-        connection = db.engine.connect()
-        df = pd.read_sql(db.session.query(ProblemLog).statement, connection)
+        logs = (
+            db.session.query(ProblemLog)
+            .options(
+                joinedload(ProblemLog.question)
+                .load_only(Question.external_id)  # type: ignore
+                .joinedload(Question.skills)  # type: ignore
+            )
+            .all()
+        )
 
+        data = []
+        columns = set(ProblemLog.__table__.columns) - {"question_id"}  # type: ignore
+        for log in logs:
+            skills_str = "_".join(sorted(skill.name for skill in log.question.skills))
+            data.append(
+                {
+                    **{c.name: getattr(log, c.name) for c in columns},
+                    "question_id": log.question.external_id,
+                    "skills": skills_str,
+                }
+            )
+
+        df = pd.DataFrame(data)
         os.makedirs(DIR, exist_ok=True)
         df.to_csv(os.path.join(DIR, "problem_logs.csv"), index=False)
 
