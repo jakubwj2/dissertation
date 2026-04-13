941548e3817bb6c266d9035db082c835d03a7f27
jakubwj2 <jakubwj2@gmail.com>
2026-04-13 12:58:22 +0100

    Add models to __init__.py

---
+++ backend/models/__init__.py
@@ -0,0 +1,20 @@
+# ensure correct import order
+from .course import Course
+from .enrollment import enrollments
+from .problem_log import ProblemLog
+from .question import Question
+from .questions_skills import questions_skills
+from .skill import Skill
+from .user import Student, Teacher, User
+
+__all__ = [
+    "Course",
+    "enrollments",
+    "ProblemLog",
+    "Question",
+    "questions_skills",
+    "Skill",
+    "Student",
+    "Teacher",
+    "User",
+]
